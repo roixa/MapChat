4709444e0cc36c2fdd7ed390f42dc1e90ef778c2
belyalov <belyalov@azoft.com>
2018-01-21 16:35:16 +0700

    use reppository template

---
+++ app/src/main/java/com/roix/mapchat/data/repositories/room/IRoomRepository.kt
@@ -0,0 +1,7 @@
+package com.roix.mapchat.data.repositories.room
+
+/**
+ * Created by roix template
+ * https://github.com/roixa/RoixArchitectureTemplates
+ */
+interface IRoomRepository 
---
+++ app/src/main/java/com/roix/mapchat/data/repositories/room/RoomRepository.kt
@@ -0,0 +1,14 @@
+package com.roix.mapchat.data.repositories.room
+
+import com.roix.mapchat.toothpick.common.ApplicationScope
+import javax.inject.Inject
+
+
+/**
+ * Created by roix template
+ * https://github.com/roixa/RoixArchitectureTemplates
+ */
+@ApplicationScope
+class RoomRepository : IRoomRepository {
+    @Inject constructor() {}
+}
