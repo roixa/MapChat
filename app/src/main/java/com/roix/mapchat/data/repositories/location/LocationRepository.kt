e4694a4c3705de6930445dbc96b3bbdf3f9bc629
belyalov <belyalov@azoft.com>
2018-03-06 22:46:08 +0700

    restart locationservice

--- app/src/main/java/com/roix/mapchat/data/repositories/location/LocationRepository.kt
+++ app/src/main/java/com/roix/mapchat/data/repositories/location/LocationRepository.kt
@@ -1,25 +1,18 @@
 package com.roix.mapchat.data.repositories.location
 
-import android.annotation.SuppressLint
-import android.app.Notification
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import com.roix.mapchat.data.models.GroupItem
-import com.roix.mapchat.toothpick.common.ApplicationScope
-import io.reactivex.Completable
-import javax.inject.Inject
-import android.app.PendingIntent
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.os.Build
-import com.roix.mapchat.R
-import com.roix.mapchat.ui.root.views.RootActivity
 import android.os.Bundle
+import android.util.Log
 import com.google.android.gms.maps.model.LatLng
+import com.roix.mapchat.data.models.GroupItem
 import com.roix.mapchat.data.repositories.firebase.FirebaseRepository
+import com.roix.mapchat.toothpick.common.ApplicationScope
 import com.roix.mapchat.utils.rx.general.RxSchedulersAbs
+import javax.inject.Inject
 
 
 /**
@@ -29,15 +22,17 @@ import com.roix.mapchat.utils.rx.general.RxSchedulersAbs
 @ApplicationScope
 class LocationRepository : ILocationRepository, LocationListener {
 
-
     @Inject lateinit var firebaseRepository: FirebaseRepository
 
-    @Inject lateinit var context: Context
+    var context: Context
     @Inject lateinit var rxScheduler: RxSchedulersAbs
 
     private lateinit var currentGroup: GroupItem
 
-    @Inject constructor()
+    @Inject constructor(context:Context){
+        this.context=context
+        context.startService(Intent(context,LocationService::class.java))
+    }
 
 
     companion object {
--- app/src/main/java/com/roix/mapchat/data/repositories/location/LocationService.kt
+++ app/src/main/java/com/roix/mapchat/data/repositories/location/LocationService.kt
@@ -3,59 +3,19 @@ package com.roix.mapchat.data.repositories.location
 import android.app.Notification
 import android.app.PendingIntent
 import android.app.Service
-import android.content.Context
 import android.content.Intent
-import android.location.Location
-import android.location.LocationManager
-import android.os.Bundle
 import android.os.IBinder
 import android.util.Log
 import com.roix.mapchat.R
 import com.roix.mapchat.data.models.GroupItem
-import com.roix.mapchat.data.repositories.firebase.FirebaseRepository
 import com.roix.mapchat.ui.root.views.RootActivity
 import toothpick.Toothpick
-import javax.inject.Inject
 
 
 /**
  * Created by belyalov on 09.02.2018.
  */
 class LocationService : Service() {
-    @Inject lateinit var mLocationManager: LocationManager
-
-    @Inject lateinit var firebaseRepository: FirebaseRepository
-
-    private lateinit var currentGroup: GroupItem
-
-    private var mLocationListeners = arrayOf(LocationListener(LocationManager.GPS_PROVIDER),
-            LocationListener(LocationManager.NETWORK_PROVIDER))
-
-    private inner class LocationListener(provider: String) : android.location.LocationListener {
-        internal var mLastLocation: Location
-
-        init {
-            Log.e(TAG, "LocationListener " + provider)
-            mLastLocation = Location(provider)
-        }
-
-        override fun onLocationChanged(location: Location) {
-            Log.e(TAG, "onLocationChanged: " + location)
-            mLastLocation.set(location)
-        }
-
-        override fun onProviderDisabled(provider: String) {
-            Log.e(TAG, "onProviderDisabled: " + provider)
-        }
-
-        override fun onProviderEnabled(provider: String) {
-            Log.e(TAG, "onProviderEnabled: " + provider)
-        }
-
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {
-            Log.e(TAG, "onStatusChanged: " + provider)
-        }
-    }
 
     override fun onBind(arg0: Intent): IBinder? {
         return null
@@ -63,14 +23,7 @@ class LocationService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Log.e(TAG, "onStartCommand")
-        super.onStartCommand(intent, flags, startId)
-        val group = intent?.extras?.getParcelable<GroupItem>(TAG_GROUP_INTENT)
-        Log.e(TAG, "on receive intent " + intent?.getStringExtra("test"))
 
-        if (group != null) {
-            currentGroup = group
-            startForeground(OUTGOING_NOTIFICATION_ID, buildPendingNotification(currentGroup))
-        }
         return START_STICKY
     }
 
@@ -78,41 +31,12 @@ class LocationService : Service() {
         Log.e(TAG, "onCreate")
         val scope = Toothpick.openScope(application)
         Toothpick.inject(this, scope)
-        try {
-            mLocationManager!!.requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER, LOCATION_INTERVAL.toLong(), LOCATION_DISTANCE,
-                    mLocationListeners[1])
-        } catch (ex: java.lang.SecurityException) {
-            Log.i(TAG, "fail to request location update, ignore", ex)
-        } catch (ex: IllegalArgumentException) {
-            Log.d(TAG, "network provider does not exist, " + ex.message)
-        }
-
-        try {
-            mLocationManager!!.requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER, LOCATION_INTERVAL.toLong(), LOCATION_DISTANCE,
-                    mLocationListeners[0])
-        } catch (ex: java.lang.SecurityException) {
-            Log.i(TAG, "fail to request location update, ignore", ex)
-        } catch (ex: IllegalArgumentException) {
-            Log.d(TAG, "gps provider does not exist " + ex.message)
-        }
 
     }
 
     override fun onDestroy() {
         Log.e(TAG, "onDestroy")
         super.onDestroy()
-        if (mLocationManager != null) {
-            for (i in mLocationListeners.indices) {
-                try {
-                    mLocationManager!!.removeUpdates(mLocationListeners[i])
-                } catch (ex: Exception) {
-                    Log.i(TAG, "fail to remove location listners, ignore", ex)
-                }
-
-            }
-        }
     }
 
     private fun buildPendingNotification(groupItem: GroupItem): Notification {
